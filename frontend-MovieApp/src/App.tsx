120c28ea77d6daaae54eccf38931b12fe3c6b693
PRAMODCompiler <pramod.compiler@gmail.com>
2024-10-15 23:00:59 +0530

    added clone

--- frontend-MovieApp/src/App.tsx
+++ frontend-MovieApp/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import { useAppDispatch, useAppSelector } from "./hooks/storeHook";
